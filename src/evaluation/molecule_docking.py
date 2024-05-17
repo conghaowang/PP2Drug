4607c734e66ae0dc66e171ac99c80b7f83794cbb
nomoresomethingwentwrong <lll109016664@outlook.com>
2024-05-17 18:54:05 +0800

    input generation res path as arg

--- src/evaluation/molecule_docking.py
+++ src/evaluation/molecule_docking.py
@@ -8,7 +8,7 @@ import subprocess
 from utils import build_pdb_dict
 
 
-def molecule_docking(bridge_type, res_path, gen_files, pdb_rev_dict):
+def molecule_docking(bridge_type, res_path, gen_files, pdb_rev_dict, root):
     for gen_file in tqdm(gen_files):
         ligand_name = gen_file.split('.')[0]
         pattern = r"(\w+_[A-Z]_rec)"
@@ -28,8 +28,11 @@ def molecule_docking(bridge_type, res_path, gen_files, pdb_rev_dict):
             print(f"Cannot find the ligand file, protein file or original ligand file for ligand {ligand_name}")
             continue
 
-        log_path = '../../docking_res/logs/' + bridge_type
-        out_path = '../../docking_res/output/' + bridge_type
+        # log_path = '../../docking_res/logs/' + bridge_type
+        # out_path = '../../docking_res/output/' + bridge_type
+
+        log_path = os.path.join(root, 'logs')
+        out_path = os.path.join(root, 'output')
         os.makedirs(log_path, exist_ok=True)
         os.makedirs(out_path, exist_ok=True)
 
@@ -40,13 +43,15 @@ def molecule_docking(bridge_type, res_path, gen_files, pdb_rev_dict):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--bridge', '-b', type=str, default='vp', help='Type of bridge to use')
+    parser.add_argument('--root', '-r', type=str, required=True, help='Path of lightning logs')
     args = parser.parse_args()
     bridge_type = args.bridge
 
-    res_path = '../../generation_results/' + bridge_type
+    # res_path = '../../generation_results/' + bridge_type
+    res_path = os.path.join(args.root, bridge_type)
     raw_data_path = '../../data/cleaned_crossdocked_data/raw'
 
     gen_files = os.listdir(res_path)
     pdb_dict, pdb_rev_dict = build_pdb_dict(raw_data_path)
 
-    molecule_docking(bridge_type, res_path, gen_files, pdb_rev_dict)
+    molecule_docking(bridge_type, res_path, gen_files, pdb_rev_dict, root=args.root)
